84d1b591cdbcc2d9c87f055c7aa0fa9b4599db4a
hanxiaoqing19910916 <190658047@qq.com>
2016-09-06 18:49:24 +0800

    增加 splitViewController

--- DingDing_Project/DingDing_Project/MainViewCtroller/HXViewController.m
+++ DingDing_Project/DingDing_Project/MainViewCtroller/HXViewController.m
@@ -8,8 +8,6 @@
 
 #import "HXViewController.h"
 #import "HXPrefixHeader.h"
-#import "HXBarButton.h"
-#import "HXSelectBar.h"
 #import "HXLeftToolBar.h"
 
 @interface HXViewController ()
@@ -17,6 +15,45 @@
 @end
 
 @implementation HXViewController
+
+/**
+ NFSplitViewController :
+ 
+ NSView* contentView = self.window.contentView;
+ 
+ self.splitViewController = [[NFSplitViewController alloc] init];
+ self.splitViewController.view.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
+ self.splitViewController.view.frame = contentView.bounds;
+ //    self.splitViewController.vertical = NO;
+ [contentView addSubview:self.splitViewController.view];
+ 
+ TestViewController *leftVc = [[TestViewController alloc] init];
+ leftVc.backgroundColor = [NSColor whiteColor];
+ leftVc.name = @"Master";
+ 
+ TestViewController* rightVc = [[TestViewController alloc] init];
+ rightVc.backgroundColor = [NSColor whiteColor];
+ rightVc.name = @"Detail right";
+ 
+ [self.splitViewController addChildViewController:leftVc];
+ [self.splitViewController addChildViewController:rightVc];
+ 
+ 
+ TestViewController :
+ 
+ - (CGFloat)minimumLengthInSplitViewController:(NFSplitViewController*)splitViewController
+ {
+ return 214;
+ }
+ 
+ - (CGFloat)maximumLengthInSplitViewController:(NFSplitViewController *)splitViewController
+ {
+ return 393;
+ }
+ 
+ 
+
+ */
 
 /**
  当从xib加载控制器view的时候，系统会调用这个方法
@@ -27,15 +64,9 @@
     if (self) {
         
         // 控制器view默认是右边和底部随父控件拉伸的 NSViewMaxXMargin|NSViewMinYMargin
-        
         // [self.view backGroundColor:[NSColor purpleColor]];
-        
-        // 测试添加自子控件
-        HXSelectBar *sBar = [[HXSelectBar alloc] initWithFrame:NSMakeRect(10, 200, 200, 200)];
-        [sBar backGroundColor:[NSColor cyanColor]];
-//        [self.view addSubview:sBar];
 
-        
+        // 添加左边工具栏 view
         HXLeftToolBar *leftBar = [HXLeftToolBar loadXibLeftToolBar];
         leftBar.userName = @"韩小青";
         [leftBar backGroundColor:HXColor(221, 228, 242)];
@@ -44,15 +75,14 @@
         [leftBar autoPinEdgeToSuperviewEdge:ALEdgeLeading withInset:0];
         [leftBar autoPinEdgeToSuperviewEdge:ALEdgeBottom withInset:0];
         [leftBar autoSetDimension:ALDimensionWidth toSize:100];
+        
+        
+        
     }
     return self;
 }
 
 
-- (void)clickBtn:(HXBarButton *)btn {
-    NSLog(@"MXButton----click");
-    btn.selected = !btn.selected;
-}
 
 
 
