204bde0d7190a7bcc6775d1f7a430773b5329119
hanxiaoqing19910916 <190658047@qq.com>
2016-09-25 23:03:36 +0800

    整理

--- DingDing_Project/DingDing_Project/Message/MiddleMessagePage/Controller/HXMiddleMessageControlller.m
+++ DingDing_Project/DingDing_Project/Message/MiddleMessagePage/Controller/HXMiddleMessageControlller.m
@@ -22,25 +22,6 @@
 
 @implementation HXMiddleMessageControlller
 
-- (void)test {
-    // 解析Plist
-    //    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"more_topics.plist" ofType:nil];
-    //    NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:filePath];
-    //    NSArray *dictArr = dict[@"list"];
-    //    // 设计模型属性代码
-    //    [NSObject createPropertyCodeWithDict:dictArr[0]];
-    
-    //    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-    //    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
-    //    [manager GET:@"https://github.com/rs" parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-    //
-    //    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-    //
-    //
-    //    }];
-    
-    
-   }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -57,8 +38,6 @@
 - (nullable NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(nullable NSTableColumn *)tableColumn row:(NSInteger)row {
     
     HXMessageCell *cell = [tableView makeViewWithIdentifier:@"msgCell" owner:self];
-    [cell backGroundColor:[NSColor redColor]];
-//    NSLog(@"%@",cell.subviews);
     return cell;
 }
 
@@ -78,6 +57,24 @@
 - (CGFloat)maximumLengthInSplitViewController:(NFSplitViewController *)splitViewController
 {
     return 275;
+}
+
+- (void)test {
+    // 解析Plist
+    //    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"more_topics.plist" ofType:nil];
+    //    NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:filePath];
+    //    NSArray *dictArr = dict[@"list"];
+    //    // 设计模型属性代码
+    //    [NSObject createPropertyCodeWithDict:dictArr[0]];
+    
+    //    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    //    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    //    [manager GET:@"https://github.com/rs" parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+    //
+    //    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+    //
+    //
+    //    }];
 }
 
 @end
