6071ba96d244e0dc9f36fb1a717a4688f64c9b58
hanxiaoqing19910916 <190658047@qq.com>
2016-09-26 18:32:19 +0800

    记录

--- DingDing_Project/DingDing_Project/Categories/HXPrefixHeader.h
+++ DingDing_Project/DingDing_Project/Categories/HXPrefixHeader.h
@@ -38,10 +38,9 @@
 
 // https://github.com/void-main/VMMagnifyComparer 图片处理 cgimageref nsimage转换 bitmap之间的关系
 
-// https://github.com/enormego/EGOCache 缓存工具类
-
 // NSImage在读取高DPI图像时的bug及解决方案 http://blog.voidmain.guru/blog/2014/07/13/nsimage-for-high-dpi-images/
 
+// https://github.com/enormego/EGOCache 缓存工具类
 
 //https://github.com/aa703463228/SWAttributedLabel  Use In Mac OS，To show Gif or Images with text
 
@@ -54,8 +53,6 @@
 #define HXColor(r, g, b) [NSColor colorWithCalibratedRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1.0]
 // 随机色
 #define HXRandomColor HXColor(arc4random_uniform(256), arc4random_uniform(256), arc4random_uniform(256))
-
-
 
 
 
